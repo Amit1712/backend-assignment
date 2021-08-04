653cbf6d483e57b34973eb8a35b48f267e62b3a1
Amit Ctaffi <ogamitctaffi@gmail.com>
2021-08-04 19:23:25 +0300

    Initalized project and connected to DB

--- src/database/index.ts
+++ src/database/index.ts
@@ -12,12 +12,12 @@ class Database {
 
   private connectToDB(): void {
     createConnection({
-      type: envString("mysql", "sqlite"),
-      host: envString(process.env.DATABASE_HOST!, ""),
-      port: envString(Number(process.env.DATABASE_PORT!), 0),
-      username: envString(process.env.DATABASE_USERNAME!, ""),
-      password: envString(process.env.DATABASE_PASSWORD!, ""),
-      database: envString(process.env.DATABASE_NAME!, "./db.sqlite"),
+      type: "postgres",
+      host: envString(process.env.DATABASE_HOST!, "localhost"),
+      port: envString(Number(process.env.DATABASE_PORT)!, 5432),
+      username: envString(process.env.DATABASE_USERNAME!, "postgres"),
+      password: envString(process.env.DATABASE_PASSWORD!, "123456"),
+      database: envString(process.env.DATABASE_NAME!, "my_db"),
       entities: [
         __dirname + "/entity/*.ts",
         __dirname + "/entity/*.js"
